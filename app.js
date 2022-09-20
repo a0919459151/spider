7106ab3f0ee55d8699df22dba7807307327b817c
joe.chiu <joe.chiu@scantrader.com>
2022-09-20 15:02:09 +0800

    get stockData

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 import Koa from 'koa'
+import Router from 'koa-router'
+import koaBody from 'koa-body'
+import { spider } from './spider.js'
 
 const app = new Koa()
-// response
-app.use(ctx => {
-    ctx.body = 'Hello Koa';
-  });
-  
-  app.listen(3000, () => {
-    console.log('Start aerver at 3000 port ...')
-  });
+const router = new Router()
+
+app.use(koaBody())
+
+router.get('/', async ctx => {
+  ctx.body = '首頁 123'
+})
+
+router.get('/stockData/:stockSymbol', async ctx => {
+  const { stockSymbol } = ctx.params
+  const result = await spider(stockSymbol)
+  ctx.response.body = JSON.stringify(result)
+  // console.log(result)
+})
+
+// router.post('/stockData:2330', ctx => {
+//   const { tickerSymbolArray } = ctx.request.body
+
+//   let resObj = {}
+//   tickerSymbolArray.forEach(element => {
+//     resObj[element] = main(element)
+//   })
+// })
+
+app.use(router.routes())
+
+app.listen(3000, () => {
+  console.log('Start aerver at 3000 port ...')
+})
--- spider.js
+++ spider.js
@@ -2,32 +2,38 @@ import got from 'got'
 import cheerio from 'cheerio'
 
 const replaceTextToEmpty = (string) => {
-    return string.toString().replace(/[\u4e00-\u9fa5]/g, "")
+  return string.toString().replace(/[\u4e00-\u9fa5]/g, '')
 }
 
-const main = async (stockCode) => {
-    const url = `https://tw.stock.yahoo.com/quote/${stockCode}`
+const spider = async (stockSymbol) => {
+  const url = `https://tw.stock.yahoo.com/quote/${stockSymbol}`
 
-    const page = await got.get(url).text()
-    const $ = cheerio.load(page);
-    const table1 = $('li[class="price-detail-item H(32px) Mx(16px) D(f) Jc(sb) Ai(c) Bxz(bb) Px(0px) Py(4px) Bdbs(s) Bdbc($bd-primary-divider) Bdbw(1px)"]')
-    const table2 = $('span[class="Fw(n) Fz(16px)--mobile Fz(14px) D(f) Ai(c) C($c-trend-down)"]')
+  const page = await got.get(url).text()
+  const $ = cheerio.load(page)
+  const stockName = $('h1[class="C($c-link-text) Fw(b) Fz(24px) Mend(8px)"]').text()
+  const table1 = $('li[class="price-detail-item H(32px) Mx(16px) D(f) Jc(sb) Ai(c) Bxz(bb) Px(0px) Py(4px) Bdbs(s) Bdbc($bd-primary-divider) Bdbw(1px)"]')
+  const table2 = $('div[class="D(f) Ai(c) H(28px)"]')
 
-    const stockData = {
-        lastPrice: replaceTextToEmpty(table1.eq(0).text()), // 股價
-        chang: replaceTextToEmpty(table1.eq(8).text()), // 漲跌
-        changPersentage: replaceTextToEmpty(table1.eq(7).text()), // 漲跌幅（％）
-        open: replaceTextToEmpty(table1.eq(1).text()), // 開盤
-        previousClose: replaceTextToEmpty(table1.eq(6).text()), // 昨收
-        highest: replaceTextToEmpty(table1.eq(2).text()), // 最高
-        lowest: replaceTextToEmpty(table1.eq(3).text()), // 最低
-        Volume: replaceTextToEmpty(table1.eq(9).text()), // 成交量（張）
-        ask: table2.eq(0).text(), // 委買價
-        bid: table2.eq(5).text() // 委賣價
+  const stockData = {
+    stockCode: stockSymbol,
+    stockName,
+    info: {
+      lastPrice: replaceTextToEmpty(table1.eq(0).text()), // 股價
+      chang: replaceTextToEmpty(table1.eq(8).text()), // 漲跌
+      changPersentage: replaceTextToEmpty(table1.eq(7).text()), // 漲跌幅（％）
+      open: replaceTextToEmpty(table1.eq(1).text()), // 開盤
+      previousClose: replaceTextToEmpty(table1.eq(6).text()), // 昨收
+      highest: replaceTextToEmpty(table1.eq(2).text()), // 最高
+      lowest: replaceTextToEmpty(table1.eq(3).text()), // 最低
+      volume: replaceTextToEmpty(table1.eq(9).text()), // 成交量（張）
+      ask: table2.eq(0).text(), // 委買價
+      bid: table2.eq(5).text() // 委賣價
     }
+  }
 
-    console.log(stockData)
+  // console.log(stockData)
+  return stockData
 }
 
-// main(2330)
-export default main
+// spider(2330)
+export { spider }
